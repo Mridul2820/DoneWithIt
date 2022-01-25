3b977be21c5d1c348742ba9d3a92727a7e5c3cbc
Mridul2820 <immridulpanda2820@gmail.com>
2022-01-26 00:43:57 +0530

    view iamge screen icons

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Image, StyleSheet, View } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 import colors from '../config/colors';
 
 const ViewImageScreen = () => {
     return (
         <View style={styles.container}>
-            <View style={styles.closeIcon}></View>
-            <View style={styles.deleteIcon}></View>
+            <View style={styles.closeIcon}>
+                <MaterialCommunityIcons 
+                    name='close'
+                    color='white'
+                    size={30}
+                />
+            </View>
+            <View style={styles.deleteIcon}>
+                <MaterialCommunityIcons 
+                    name='trash-can-outline'
+                    color='white'
+                    size={30}
+                />
+            </View>
             <Image 
                 resizeMode="contain"
                 style={styles.image}
@@ -19,23 +32,17 @@ const ViewImageScreen = () => {
 
 const styles = StyleSheet.create({
     closeIcon: {
-        width: 50,
-        height: 50,
-        backgroundColor: colors.primary,
         position: 'absolute',
         top: 40,
         left: 30,
     },
     deleteIcon: {
-        width: 50,
-        height: 50,
-        backgroundColor: colors.secondary,
         position: 'absolute',
         top: 40,
         right: 30,
     },
     container: {
-        backgroundColor: colors.white,
+        backgroundColor: colors.black,
         flex: 1
     },
     image: {
