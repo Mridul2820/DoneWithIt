91ec25535974fa09d0ecc7561f0a0a4168563b77
Mridul2820 <immridulpanda2820@gmail.com>
2022-01-25 20:40:05 +0530

    text component

--- App.js
+++ App.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import AppText from './app/components/AppText';
+
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WellcomeScreen from './app/screens/WellcomeScreen';
 
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
 export default function App() {
     return (
-        <ViewImageScreen />
+        <View style={styles.container}>
+            <AppText>React Native</AppText>
+            <MaterialCommunityIcons 
+                name="email" 
+                size={60}
+            />
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    box: {
+        backgroundColor: 'dodgerblue',
+        width: 100,
+        height: 100,
+        borderWidth: 10,
+        borderRadius: 50,
+        borderColor: 'royalblue'
+    }
+})
---
+++ app/components/AppText.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Platform, StyleSheet, Text } from 'react-native'
+
+const AppText = ({ children }) => {
+    return (
+        <Text style={styles.text}>
+            {children}
+        </Text>
+    )
+}
+
+const styles = StyleSheet.create({
+    text: {
+        color: 'tomato',
+        ...Platform.select({
+            ios: {
+                fontSize: 20,
+                fontFamily: "Avenir"
+            },
+            android : {
+                fontSize: 20,
+                fontFamily: "Roboto"
+            }
+        })
+    }
+})
+
+export default AppText
