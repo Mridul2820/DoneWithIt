94eacd3d140444d3d1e6e873cf54a09ce3f11b34
Mridul2820 <immridulpanda2820@gmail.com>
2022-01-26 20:03:44 +0530

    Listing screen

--- App.js
+++ App.js
@@ -6,16 +6,16 @@ import AppText from './app/components/AppText';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WellcomeScreen from './app/screens/WellcomeScreen';
 
-import { MaterialCommunityIcons } from "@expo/vector-icons";
 import Card from './app/components/Card';
 import ListingDetailScreen from './app/screens/ListingDetailScreen';
 import MessageScreen from './app/screens/MessageScreen';
 import Screen from './app/components/Screen';
 import Icon from './app/components/Icon';
 import AccountScreen from './app/screens/AccountScreen';
+import ListingScreen from './app/screens/ListingScreen';
 
 export default function App() {
     return (
-        <AccountScreen />
+        <ListingScreen />
     );
 }
